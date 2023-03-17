caa12118817eb6581dc5a42f942eb3ffeff6d8f3
Magnus <magnus@magnuss-mbp.lan>
2023-03-17 16:17:24 +0100

    works multiple times

--- contentScript.js
+++ contentScript.js
@@ -3,14 +3,14 @@ function checkForNewElement(selector, callback) {
     let prevCount = elements.length;
 
     const observer = new MutationObserver((mutations) => {
-
-
-        console.log('TICK')
         elements = document.querySelectorAll(selector);
         if (elements.length > prevCount) {
-            console.log("REVEL")
+            console.log("UP")
             prevCount = elements.length;
             callback();
+        } else if (elements.length < prevCount) {
+            console.log("DOWN")
+            prevCount = elements.length;
         }
     });
 
@@ -22,41 +22,30 @@ function checkForNewElement(selector, callback) {
     return observer;
 }
 
-function generateRandomString(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-}
-
 function captureCanvas() {
     const canvas = document.querySelector('canvas');
     if (!canvas) return;
 
     canvas.toBlob((blob) => {
         const url = URL.createObjectURL(blob);
-        console.log('NO CANVAS')
         if (!canvas) return;
 
+        const today = new Date();
+        const dateStr = today.toISOString().slice(0, 10);
+
+        const word = document.querySelector('.word').textContent
         const a = document.createElement('a');
         a.href = url;
-        a.download = generateRandomString(10) + '.png';
+        a.download = `scribble-capture-${dateStr}-${word}.png`;
         a.style.display = 'none';
         console.log('DOWNLOADING')
         document.body.appendChild(a);
         console.log('CLICK')
         a.click();
-        setTimeout(() => {
-            document.body.removeChild(a);
-            URL.revokeObjectURL(url);
-            console.log('Remove')
-        }, 100);
+        document.body.removeChild(a);
     });
 }
 
 console.log('STARTING')
-
 const targetElementSelector = '.reveal.show'; // Replace with the selector of the element you want to track
 checkForNewElement(targetElementSelector, captureCanvas);
